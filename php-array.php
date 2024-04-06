07e07c953fe14708f389a91e96582716b2c38964
Mohamed A. Mahfouz <moa.mahfouz@gmail.com>
2024-04-06 23:24:56 +0200

    Create php-array.php

---
+++ php-array.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+'middle_east_nationalities' => [
+    'algerian' => 'جزائري',
+    'bahraini' => 'بحريني',
+    'egyptian' => 'مصري',
+    'iranian' => 'إيراني',
+    'iraqi' => 'عراقي',
+    'israeli' => 'إسرائيلي',
+    'jordanian' => 'أردني',
+    'kuwaiti' => 'كويتي',
+    'lebanese' => 'لبناني',
+    'libyan' => 'ليبي',
+    'moroccan' => 'مغربي',
+    'omani' => 'عماني',
+    'palestinian' => 'فلسطيني',
+    'qatari' => 'قطري',
+    'saudi_arabian' => 'سعودي',
+    'sudanese' => 'سوداني',
+    'syrian' => 'سوري',
+    'tunisian' => 'تونسي',
+    'emirati' => 'إماراتي',
+    'yemeni' => 'يمني',
+],
+
+
